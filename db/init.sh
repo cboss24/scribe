2b14c5ad049ca221de192217410ccc1345badf0b
Cole Bosmann <cole.bosmann@gmail.com>
2018-03-28 16:26:38 -0700

    Now inserting all batch fields into postgres, serializing nested fields

--- db/init.sh
+++ db/init.sh
@@ -10,10 +10,22 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
 	CREATE SEQUENCE job_id_sequence;
     CREATE TYPE job_status AS ENUM ('SUBMITTED', 'PENDING', 'RUNNABLE', 'STARTING', 'RUNNING', 'SUCCEEDED', 'FAILED');
 	CREATE TABLE job (
-		job_id 			bigint NOT NULL DEFAULT nextval('job_id_sequence') PRIMARY KEY,
-		batch_id		uuid NOT NULL UNIQUE,
-		last_changed	timestamptz NOT NULL,
-		status			job_status NOT NULL
+		id 				bigint NOT NULL DEFAULT nextval('job_id_sequence') PRIMARY KEY,
+		attempts		text NOT NULL,
+		container		text NOT NULL,
+		created_at 		timestamptz NOT NULL,
+		depends_on		text NOT NULL,
+		job_definition 	text NOT NULL,
+		job_id 			uuid NOT NULL UNIQUE,
+		job_name		text NOT NULL,
+		job_queue		text NOT NULL,
+		last_changed 	timestamptz NOT NULL,
+		parameters		text NOT NULL,
+		retry_strategy	text,
+		started_at 		timestamptz,
+		status 			job_status NOT NULL,
+		status_reason	text,
+		stopped_at		timestamptz
 	);
-	ALTER SEQUENCE job_id_sequence OWNED BY job.job_id;
+	ALTER SEQUENCE job_id_sequence OWNED BY job.id;
 EOSQL
--- src/worker.go
+++ src/worker.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	_ "database/sql"
+	"encoding/json"
 	"fmt"
 	"github.com/jmoiron/sqlx"
-	_ "github.com/lib/pq"
 	"log"
+	"strings"
 	"time"
 )
 
@@ -54,9 +54,44 @@ var PreviousStates map[string][]string = map[string][]string{
 }
 
 type JobRecord struct {
-	BatchId     string    `db:"batch_id"`
-	Status      string    `db:"status"`
-	LastChanged time.Time `db:"last_changed"`
+	Attempts      []byte   `db:"attempts"`
+	Container     []byte   `db:"container"`
+	CreatedAt     time.Time `db:"created_at"`
+	DependsOn     []byte   `db:"depends_on"`
+	JobDefinition *string   `db:"job_definition"`
+	JobId         *string   `db:"job_id"`
+	JobName       *string   `db:"job_name"`
+	JobQueue      *string   `db:"job_queue"`
+	LastChanged   time.Time `db:"last_changed"`
+	Parameters    []byte   `db:"parameters"`
+	RetryStrategy []byte   `db:"retry_strategy"`
+	StartedAt     time.Time `db:"started_at"`
+	Status        *string   `db:"status"`
+	StatusReason  *string   `db:"status_reason"`
+	StoppedAt     time.Time `db:"stopped_at"`
+}
+
+func NewQueryArg(e BatchEvent) QueryArg {
+	return QueryArg{
+		JobRecord: JobRecord{
+			Attempts:      Marshal(e.Detail.Attempts),
+			Container:     Marshal(e.Detail.Container),
+			CreatedAt:     Unix(e.Detail.CreatedAt),
+			DependsOn:     Marshal(e.Detail.DependsOn),
+			JobDefinition: e.Detail.JobDefinition,
+			JobId:         e.Detail.JobId,
+			JobName:       e.Detail.JobName,
+			JobQueue:      e.Detail.JobQueue,
+			LastChanged:   e.Time,
+			Parameters:    Marshal(e.Detail.Parameters),
+			RetryStrategy: Marshal(e.Detail.RetryStrategy),
+			StartedAt:     Unix(e.Detail.StartedAt),
+			Status:        e.Detail.Status,
+			StatusReason:  e.Detail.StatusReason,
+			StoppedAt:     Unix(e.Detail.StoppedAt),
+		},
+		Overwrite: PreviousStates[*e.Detail.Status],
+	}
 }
 
 type QueryArg struct {
@@ -64,18 +99,59 @@ type QueryArg struct {
 	Overwrite []string `db:"overwrite"`
 }
 
-func updateJobRecord(db *sqlx.DB, e BatchEvent) {
-	statement := `
-	INSERT INTO job (batch_id, status, last_changed)
-	VALUES (:batch_id, :status, :last_changed)
-	ON CONFLICT (batch_id) DO
-		UPDATE SET status = :status, last_changed = :last_changed
-		WHERE job.last_changed < :last_changed OR (job.last_changed = :last_changed AND job.status IN (:overwrite))
-	;`
-	input := QueryArg{
-		JobRecord: JobRecord{BatchId: *e.Detail.JobId, Status: *e.Detail.Status, LastChanged: e.Time},
-		Overwrite: PreviousStates[*e.Detail.Status],
+func Marshal(v interface{}) []byte {
+	b, err := json.Marshal(v)
+	if err != nil {
+		log.Fatalln(err)
 	}
+	return b
+}
+
+func Unix(sec *int64) time.Time {
+	if sec == nil {
+		return time.Unix(0, 0)
+	}
+	return time.Unix(*sec, 0)
+}
+
+func updateJobRecord(db *sqlx.DB, e BatchEvent) {
+
+	columns := []string{
+		"attempts", "container", "created_at", "depends_on", "job_definition", "job_id",
+		"job_name", "job_queue", "last_changed", "parameters", "retry_strategy", "status",
+	}
+
+	switch *e.Detail.Status {
+	case "SUCCEEDED", "FAILED":
+		columns = append(columns, []string{"started_at", "status_reason", "stopped_at"}...)
+	case "RUNNING":
+		columns = append(columns, []string{"started_at"}...)
+	}
+
+	params := make([]string, len(columns))
+	for k, v := range columns {
+		params[k] = ":" + v
+	}
+
+	update := make([]string, len(columns))
+	for k, v := range columns {
+		update[k] = v + " = :" + v
+	}
+
+	columnCsv := strings.Join(columns, ", ")
+	paramCsv := strings.Join(params, ", ")
+	updateCsv := strings.Join(update, ", ")
+
+	statement := fmt.Sprintf(`
+		INSERT INTO job (%s)
+		VALUES (%s)
+		ON CONFLICT (job_id) DO
+			UPDATE SET %s
+			WHERE job.last_changed < :last_changed
+			OR (job.last_changed = :last_changed AND job.status IN (:overwrite))
+		;`, columnCsv, paramCsv, updateCsv)
+	input := NewQueryArg(e)
+
 	query, args, err := sqlx.Named(statement, input)
 	if err != nil {
 		log.Fatalln(err)
